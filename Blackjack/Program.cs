831bf6d08848138086b8196cdaceec198d441905
Tue Munch Madsen <tuepvm@gmail.com>
2022-08-30 23:19:10 +0200

    Changes

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -2,17 +2,13 @@
 {
     internal class Program
     {
-        int[,] cards = new int[4, 13];
+
 
         static void Main(string[] args)
         {
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-
-                }
-            }
+            Cards cards = new Cards();
+            Console.WriteLine($"{cards.Deck}");
+            Console.WriteLine("");
         }
     }
 }
